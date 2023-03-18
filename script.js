4000f595c5391bbd39843f7e55239522845ac672
ArtemTyutyun <tutunartem13@gmail.com>
2023-03-18 15:55:47 +0200

    JS-lesson-22

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ let secondNumber;
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number");
 
+if(firstNumber == '' || secondNumber == ''){
+alert("Error"); 
+alert(breakpoint);
+}
+
 alert("Adding = " + " " + (Number(firstNumber) + Number(secondNumber)));
+
+if(firstNumber < secondNumber) {
+    alert("Are you sure you want to proceed with the operation?");
+    }  
 alert("Subtraction = " + " " + (firstNumber - secondNumber));
+
 alert("Multiplication = " + " " + (firstNumber * secondNumber));
+
+if(secondNumber == 0) {
+    alert("Division by 0 is impossible");
+}
 alert("Division = " + " " + (firstNumber / secondNumber));
+
+
 
