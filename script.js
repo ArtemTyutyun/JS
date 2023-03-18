cb68c83ea61c5ca61a335e16f271142452dfa7f9
ArtemTyutyun <tutunartem13@gmail.com>
2023-03-18 10:32:48 +0200

    lesson-21

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = prompt("Enter first number");
+secondNumber = prompt("Enter second number");
+
+alert("Adding = " + " " + (Number(firstNumber) + Number(secondNumber)));
+alert("Subtraction = " + " " + (firstNumber - secondNumber));
+alert("Multiplication = " + " " + (firstNumber * secondNumber));
+alert("Division = " + " " + (firstNumber / secondNumber));
+
