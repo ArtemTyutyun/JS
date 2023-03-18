799cce483b7d383d57fc4d71f61f1facd71976c5
ArtemTyutyun <tutunartem13@gmail.com>
2023-03-18 22:07:28 +0200

    Working with arrays and loops

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-let firstNumber;
-let secondNumber;
+// adding numbers //
+const numbers = [
+    28215,
+    27049, 
+    22591,
+    'namber', 
+    20017, 
+    15372, 
+    undefined, 
+    22486,
+    'test', 
+    24795, 
+    23914,
+    16191, 
+    15355
+]
 
-firstNumber = prompt("Enter first number");
-secondNumber = prompt("Enter second number");
+let sum = 0;
+const elementType = [];
 
-if(firstNumber == '' || secondNumber == ''){
-alert("Error"); 
-alert(breakpoint);
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  
+  if (typeof element === 'number' && !isNaN(element)){
+    elementType.push(element);
+    sum += element;
+  }
 }
 
-alert("Adding = " + " " + (Number(firstNumber) + Number(secondNumber)));
+console.log(elementType);
+console.log(sum);
 
-if(firstNumber < secondNumber) {
-    alert("Are you sure you want to proceed with the operation?");
-    }  
-alert("Subtraction = " + " " + (firstNumber - secondNumber));
 
-alert("Multiplication = " + " " + (firstNumber * secondNumber));
 
-if(secondNumber == 0) {
-    alert("Division by 0 is impossible");
+
+
+// min/max nambers //
+let min = numbers[0];
+let max = numbers[0];
+const elementType2 = [];
+
+for (let i = 1; i < numbers.length; i++) {
+  const element = numbers[i];
+
+  if (element < min) {
+    min = element;
+  }
+  if (element > max) {
+    max = element;
+  }
+
+  if (typeof numbers[i] === 'number' && !isNaN(numbers[i])) {
+    elementType2.push(numbers[i]);
+  }
 }
-alert("Division = " + " " + (firstNumber / secondNumber));
+
+console.log(elementType2);
+console.log("Мінімальне значення: " + min);
+console.log("Максимальне значення: " + max);
 
 
 
+
+
+
+// lattices as a numbers //
+for( let i = 0; i <= 5; i++) {
+    let lattices = "";
+    for(let j = 1; j <= i; j++) {
+        lattices += "#";
+    }
+    console.log(lattices);
+}
