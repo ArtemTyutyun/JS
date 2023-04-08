fc0e1efe79075d6d922fcc68ab6001dc37faa6d1
ArtemTyutyun <tutunartem13@gmail.com>
2023-04-08 20:12:59 +0300

    Basics of JS. Comparison operators. Conditional operators. Logical operators

--- script.js
+++ script.js
@@ -4,24 +4,22 @@ let secondNumber;
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number");
 
-if(firstNumber == '' || secondNumber == ''){
-alert("Error"); 
-alert(breakpoint);
+if (firstNumber === "" || secondNumber === "") {
+  alert("Error: You need to enter two numbers");
+  breakpoint;
 }
 
-alert("Adding = " + " " + (Number(firstNumber) + Number(secondNumber)));
+alert(`Adding = ${Number(firstNumber) + Number(secondNumber)}`);
 
-if(firstNumber < secondNumber) {
-    alert("Are you sure you want to proceed with the operation?");
-    }  
-alert("Subtraction = " + " " + (firstNumber - secondNumber));
-
-alert("Multiplication = " + " " + (firstNumber * secondNumber));
-
-if(secondNumber == 0) {
-    alert("Division by 0 is impossible");
+if (firstNumber < secondNumber) {
+  alert("Are you sure you want to proceed with the operation ?");
 }
-alert("Division = " + " " + (firstNumber / secondNumber));
+alert(`Subtraction =  ${firstNumber - secondNumber}`);
 
+alert(`Multiplication =  ${firstNumber * secondNumber}`);
 
-
+if (secondNumber == 0) {
+  alert("Division by 0 is impossible");
+} else {
+  alert(`Division = ${firstNumber / secondNumber}`);
+}
