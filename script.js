bb790c3b263c278d19e474f8a2b91ad0c4a80464
ArtemTyutyun <tutunartem13@gmail.com>
2023-04-20 23:08:04 +0300

    JS. Working with the DOM. Communication with elements on the page

--- script.js
+++ script.js
@@ -1,54 +1,127 @@
-const result = document.getElementById("result");
+let firstNumber = prompt("Enter first number");
+let secondNumber = prompt("Enter second number");
 
-function add() {
-	const num1 = parseFloat(document.getElementById("number1").value);
-	const num2 = parseFloat(document.getElementById("number2").value);
-	const sum = num1 + num2;
-	result.textContent = sum;
-	if (sum >= 0) {
-		result.style.color = `rgb(0, ${Math.min(sum * 10, 255)}, 0)`;
-	} else {
-		result.style.color = `rgb(${Math.min(-sum * 10, 255)}, 0, 0)`;
-	}
+function error(a, b) {
+  if (a === '' || b === '') {
+    throw("Error: Please enter a number");
+  }
 }
 
-function subtract() {
-	const num1 = parseFloat(document.getElementById("number1").value);
-	const num2 = parseFloat(document.getElementById("number2").value);
-	const difference = num1 - num2;
-	result.textContent = difference;
-	if (difference >= 0) {
-		result.style.color = `rgb(0, ${Math.min(difference * 10, 255)}, 0)`;
-	} else {
-		result.style.color = `rgb(${Math.min(-difference * 10, 255)}, 0, 0)`;
-	}
+function adding(a, b) {
+  if (!isNaN(a) && !isNaN(b)) {
+    const result = a + b;
+    let color = result >= 0 ? "green" : "red";
+    return "Adding = <span style='color: " + color + "'>" + result + "</span>";
+  } else {
+    return "<span style='color:red;'>Error: Not a number</span>";
+  }
 }
 
-function multiply() {
-	const num1 = parseFloat(document.getElementById("number1").value);
-	const num2 = parseFloat(document.getElementById("number2").value);
-	const product = num1 * num2;
-	result.textContent = product;
-	if (product >= 0) {
-		result.style.color = `rgb(0, ${Math.min(product * 10, 255)}, 0)`;
-	} else {
-		result.style.color = `rgb(${Math.min(-product * 10, 255)}, 0, 0)`;
-	}
+function subtracting(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return "<span style='color:red;'>Error: Not a number</span>";
+  }
+  if (a < b) {
+    if (confirm("Are you sure you want to proceed with the operation?")) {
+      const result = a - b;
+      let color = result >= 0 ? "green" : "red"; 
+      return "Subtracting = <span style='color: " + color + "'>" + result + "</span>";
+    } else {
+      return "<span style='color:red;'>The subtraction was canceled</span>";
+    }
+  } else {
+    const result = a - b;
+    let color = result >= 0 ? "green" : "red";
+    return "Subtracting = <span style='color: " + color + "'>" + result + "</span>";
+  }
 }
 
-function divide() {
-	const num1 = parseFloat(document.getElementById("number1").value);
-	const num2 = parseFloat(document.getElementById("number2").value);
-	const quotient = num1 / num2;
-	result.textContent = quotient;
-	if (quotient >= 0) {
-		result.style.color = `rgb(0, ${Math.min(quotient * 10, 255)}, 0)`;
-	} else {
-		result.style.color = `rgb(${Math.min(-quotient * 10, 255)}, 0, 0)`;
-	}
+function multiplying(a, b) {
+  if (!isNaN(a) && !isNaN(b)) {
+    const result = a * b;
+    let color = result >= 0 ? "green" : "red"; 
+    return "Multiplying = <span style='color: " + color + "'>" + result + "</span>";
+  } else {
+    return "<span style='color:red;'>Error: Not a number</span>";
+  } 
 }
 
-document.getElementById("addBtn").addEventListener("click", add);
-document.getElementById("subtractBtn").addEventListener("click", subtract);
-document.getElementById("multiplyBtn").addEventListener("click", multiply);
-document.getElementById("divideBtn").addEventListener("click", divide);
+function dividing(a, b) {
+  if (!isNaN(a) && !isNaN(b)) {
+    const result = a / b;
+    if (b == 0) {
+		return "<span style='color:red;'>Error: Division by 0 is impossible</span>";
+    } else {
+      let color = result >= 0 ? "green" : "red"; 
+      return "Dividing = <span style='color: " + color + "'>" + result + "</span>";
+    }
+  } else {
+    return "<span style='color:red;'>Error: Not a number</span>";
+  }
+}
+
+error(firstNumber, secondNumber);
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+document.write(adding(firstNumber, secondNumber) + "<br>");
+document.write("<br>");
+document.write(subtracting(firstNumber, secondNumber) + "<br>");
+document.write("<br>");
+document.write(multiplying(firstNumber, secondNumber) + "<br>");
+document.write("<br>");
+document.write(dividing(firstNumber, secondNumber) + "<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+// min/max nambers //
+const numbers = [
+	27049,
+	true,
+	46066,
+	22591,
+	false,
+	20232, 
+	15372,  
+	null, 
+	66230,
+	24795, 
+	'test',
+	14433,
+	undefined,
+	45986,
+	15355
+  ];
+  
+  function findMinMax(numbers) {
+	let min = Infinity;
+	let max = -Infinity;
+  
+	for (let i = 0; i < numbers.length; i++) {
+	  const element = numbers[i];
+	  if(typeof element === 'number') {
+		if (element < min) {
+		  min = element;
+		}
+  
+		if (element > max) {
+		  max = element;
+		}
+	  }
+	}
+	return [min, max];
+  }
+  
+  const [min, max] = findMinMax(numbers);
+  
+  const fixedMin = 0;
+  const fixedMax = 100000; 
+  
+  const colorMin = `rgb(${Math.round(((min - fixedMin) / (fixedMax - fixedMin)) * 255)}, 0, 0)`;
+  const colorMax = `rgb(0, ${Math.round(((max - fixedMin) / (fixedMax - fixedMin)) * 255)}, 0)`;
+  
+  document.write("Finding the minimum and maximum values: <br>");
+  document.write("<br>");
+  document.write(`<span style="color: ${colorMin}">Minimum value: ${min}</span> <br>`);
+  document.write(`<span style="color: ${colorMax}">Maximum value: ${max}</span> <br>`);
