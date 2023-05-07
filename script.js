e555a84a6ac0479fb08bcd00bc646dc324e5cb35
ArtemTyutyun <tutunartem13@gmail.com>
2023-05-07 19:55:09 +0300

    Communication with the server

--- script.js
+++ script.js
@@ -25,3 +25,51 @@ document.addEventListener('keydown', function(event) {
     section.scrollIntoView({behavior: "smooth"});
   }
 });
+
+
+
+
+// gitHub links //
+window.addEventListener('load', function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.github.com/users/ArtemTyutyun/repos');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var repos = JSON.parse(xhr.responseText);
+      showProjects(repos);
+    } else {
+      console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();
+});
+
+
+function showProjects(repos) {
+  var projectList = document.getElementById('project-list');
+  for (var i = 0; i < repos.length; i++) {
+    var repo = repos[i];
+    var li = document.createElement('li');
+    li.setAttribute('style', 'margin-left: 80px;')
+    var a = document.createElement('a');
+    a.setAttribute('href', repo.html_url);
+    a.setAttribute('style', 'color: blue; text-decoration: none;');
+    a.addEventListener('mouseover', function() {
+        this.style.color = 'green';
+    });
+    a.addEventListener('mouseout', function() {
+        this.style.color = 'blue';
+    });
+    a.addEventListener('click', function() {
+      this.style.color = 'red';
+  });
+    a.appendChild(document.createTextNode(repo.full_name));
+    li.appendChild(a);
+    if (repo.description) {
+      var p = document.createElement('p');
+      p.appendChild(document.createTextNode(repo.description));
+      li.appendChild(p);
+    }
+    projectList.appendChild(li);
+  }
+}
